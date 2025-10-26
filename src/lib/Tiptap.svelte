d22695533ad6676bc1f9bad77ffda5092b2f96b6
Jonas Olofsson <jonas_emailbox@yahoo.co.uk>
2025-10-25 21:21:41 -0300

    separate menubar

--- src/lib/Tiptap.svelte
+++ src/lib/Tiptap.svelte
@@ -3,6 +3,7 @@
 	import { Editor } from '@tiptap/core';
 	import { StarterKit } from '@tiptap/starter-kit';
 	import BubbleMenu from '@tiptap/extension-bubble-menu';
+	import TiptapMenu from './TiptapMenu.svelte';
 
 	let {
 		content = `
@@ -39,34 +40,11 @@
 
 <div style="position: relative" class="app">
 	{#if editor}
-		<div class="fixed-menu">
-			<button
-				onclick={() => editor?.chain().focus().toggleHeading({ level: 1 }).run()}
-				class:active={editor?.isActive('heading', { level: 1 })}
-			>
-				H1
-			</button>
-			<button
-				onclick={() => editor?.chain().focus().toggleHeading({ level: 2 }).run()}
-				class:active={editor?.isActive('heading', { level: 2 })}
-			>
-				H2
-			</button>
-			<button
-				onclick={() => editor?.chain().focus().setParagraph().run()}
-				class:active={editor?.isActive('paragraph')}
-			>
-				P
-			</button>
-		</div>
+		<TiptapMenu {editor} />
 	{/if}
 
 	<div bind:this={element}></div>
 </div>
 
 <style>
-	button.active {
-		background: black;
-		color: white;
-	}
 </style>
---
+++ src/lib/TiptapMenu.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Editor } from '@tiptap/core';
+
+	let { editor }: { editor: Editor } = $props();
+</script>
+
+<div class="fixed-menu">
+	<button
+		onclick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+		class:active={editor.isActive('heading', { level: 1 })}
+	>
+		H1
+	</button>
+	<button
+		onclick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+		class:active={editor.isActive('heading', { level: 2 })}
+	>
+		H2
+	</button>
+	<button
+		onclick={() => editor.chain().focus().setParagraph().run()}
+		class:active={editor.isActive('paragraph')}
+	>
+		P
+	</button>
+</div>
+
+<style>
+	button.active {
+		background: black;
+		color: white;
+	}
+</style>
